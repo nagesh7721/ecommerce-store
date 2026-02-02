74110b93d100ba46709299c8e3792141de28932f
unknown <nagesh7721@gmail.com>
2026-02-02 22:18:31 +0530

    Frontend pages added and updated

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const protect = require("../middleware/authMiddleware");
-// const admin = require("../middleware/adminMiddleware");
-
-// const {
-//   addProduct,
-//   getProducts,
-// } = require("../controllers/productController");
-
-// // ✅ Public
-// router.get("/", getProducts);
-
-// // ✅ Admin
-// router.post("/", protect, admin, addProduct);
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 
